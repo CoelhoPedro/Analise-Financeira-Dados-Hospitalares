f6132393cd8d1437e31e5afdcabc8b8cbc94b0f2
Pedro Henrique Coelho Ferreira <pedrohenrique.mpa@gmail.com>
2023-03-20 16:37:05 -0300

    Atualizando analises sql

--- Projeto.R
+++ Projeto.R
@@ -31,57 +31,66 @@ dados$Raça = as.factor(dados$Raça)
 # Pergunta 1:
 # Quantas raças estão representadas no dataset?
 
-sqldf("select distinct Raça from dados")
+num_racas = sqldf("select Raça, count(Raça) as NumeroRaças from dados group by Raça")
+num_racas
 
 # Existem 6 raças no dataset
 # Pergunta 2:
 # Qual a idade média dos pacientes?
 
-sqldf("select avg(Idade) from dados")
+idade_media = sqldf("select avg(Idade) from dados")
+idade_media
 
 # A média de idade é de 5 anos
 # Pergunta 3:
 # Qual a moda da idade dos pacientes?
 
-sqldf("select Idade, count(*) from dados group by Idade")
+idade_moda = sqldf("select Idade, count(*) as Contagem from dados group by Idade order by Contagem desc limit 1")
+idade_moda
 
 # A idade que mais aparece é 0, com 306 registros
 # Pergunta 4:
 # Qual a variância da coluna idade?
 
-sqldf("select variance(Idade) from dados")
+idade_variancia = sqldf("select variance(Idade) as Variancia from dados")
+idade_variancia
 
 # A variância da idade é 48,34
 # Pergunta 5:
 # Qual o gasto total com internações hospitalares por idade?
 
-sqldf("select Idade, sum(CustoInternaçao) as Custo from dados group by Idade")
+gasto_total_idade = sqldf("select Idade, sum(CustoInternaçao) as Custo from dados group by Idade")
+gasto_total_idade
 
 # Pergunta 6
 # Qual idade gera o maior gasto total com internações hospitalares?
 
-sqldf("select Idade, max(Custo) from (select Idade, sum(CustoInternaçao) as Custo from dados group by Idade)")
+gasto_total_idade_maior = sqldf("select Idade, max(Custo) as Custo from (select Idade, sum(CustoInternaçao) as Custo from dados group by Idade)")
+gasto_total_idade_maior
 
 # A idade que mais gera gasto total é a idade 0
 # Pergunta 7
 # Qual o gasto total com internações hospitalares por gênero?
 
-sqldf("select Sexo, sum(CustoInternaçao) as Custo from dados group by Sexo")
+gasto_total_genero = sqldf("select Sexo, sum(CustoInternaçao) as Custo from dados group by Sexo")
+gasto_total_genero
 
 # O sexo masculino gera mais gastos do que o feminino
 # Pergunta 8
 # Qual a média de gasto com internações hospitalares por raça do paciente?
 
-sqldf("select Raça, avg(CustoInternaçao) as MediaCusto from dados group by Raça")
+gasto_medio_raca = sqldf("select Raça, avg(CustoInternaçao) as MediaCusto from dados group by Raça")
+gasto_medio_raca
 
 # Pergunta 9
 # Para  pacientes  acima  de  10  anos,  qual  a  média  de  gasto  total  com  internações hospitalares?
 
-sqldf("select Idade, avg(CustoInternaçao) as MediaCusto from dados where Idade > 10 group by Idade")
-sqldf("select avg(CustoInternaçao) as MediaCusto from dados where Idade > 10")
+gasto_medio_idade_acima10 = sqldf("select Idade, avg(CustoInternaçao) as MediaCusto from dados where Idade > 10 group by Idade")
+gasto_medio_idade_acima10
 
 # A média de gasto com internações para pacientes acima de 10 anos é 3.213,66
 # Pergunta 10
 # Considerando o item anterior, qual idade tem média de gastos superior a 3000?
 
-sqldf("Select Idade, MediaCusto from (select Idade, avg(CustoInternaçao) as MediaCusto from dados where Idade>10 group by Idade) where MediaCusto > 3000")
+gasto_medio_idade_acima10_maior3k = sqldf("Select Idade, avg(CustoInternaçao) as MediaCusto from dados where Idade > 10 group by Idade having MediaCusto > 3000")
+gasto_medio_idade_acima10_maior3k
