172be71905ca34ea4c169c106d76e90d44e05f53
Pedro Henrique Coelho Ferreira <pedrohenrique.mpa@gmail.com>
2023-03-21 11:44:15 -0300

    Respondendo as perguntas com analises estatisticas

--- Projeto.R
+++ Projeto.R
@@ -111,3 +111,123 @@ ggplot(dados, aes(x = Idade)) +
 
 # Pergunta 2
 # Qual faixa etária tem o maior gasto total no hospital?
+?aggregate
+gasto_total_faixa_etaria = aggregate(data = dados,
+                                     CustoInternaçao ~ Idade,
+                                     FUN = sum)
+gasto_total_faixa_etaria
+
+which.max(tapply(gasto_total_faixa_etaria$CustoInternaçao,
+                 gasto_total_faixa_etaria$CustoInternaçao,
+                 FUN = sum))
+
+barplot(tapply(gasto_total_faixa_etaria$CustoInternaçao,
+               gasto_total_faixa_etaria$Idade,
+               FUN = sum))
+
+# A faixa etária que possui o maior gasto total é entre 0 e 1 ano
+# Pergunta 3
+# Qual grupo baseado em diagnóstico (Aprdrg) tem o maior gasto total no hospital?
+
+gasto_total_grupo = aggregate(data = dados,
+                                     CustoInternaçao ~ GrupoDiagnosticoRefinado,
+                                     FUN = sum)
+gasto_total_grupo
+gasto_total_grupo[which.max(gasto_total_grupo$CustoInternaçao),]
+
+# O grupo que possui o maior gasto é o 640
+# Pergunta 4
+# A raça do paciente tem relação com o total gasto em internações no hospital?
+
+# Vamos usar o teste ANOVA
+# Variável dependente: CustoInternaçao
+# Variável independente: Raça
+
+# Hipóteses:
+# H0 - A variável Raça não afeta a variável CustoInternação
+# H1 - A variável Raça afeta a variável CustoInternação
+
+teste_anova = aov(CustoInternaçao ~ Raça, data = dados)
+summary(teste_anova)
+
+# Conclusão:
+# Valor-p > 0.05, logo, falhamos em rejeitar a H0.
+# A raça do paciente não influencia no total gasto com internações no hospital
+
+# Pergunta 5
+# A combinação de idade e gênero dos pacientes influencia no gasto total em internações no hospital?
+
+# Vamos usar o teste ANOVA mais uma vez
+# Variável dependente: CustoInternaçao
+# Variáveis independentes: Idade, Sexo
+
+# Hipóteses:
+# H0 - As variáveis Idade e Sexo não afetam a variável CustoInternação
+# H1 - As variáves Idade e Sexo afetam a variável CustoInternação
+
+teste_anova_2 = aov(CustoInternaçao ~ Idade + Sexo, data = dados)
+summary(teste_anova_2)
+
+# Conclusão:
+# Valor-p < 0.05 para as duas variáveis independentes, logo, rejeitamos a H0.
+# A raça e o sexo do paciente influenciam significativamente no total gasto com internações no hospital
+
+# Pergunta 6
+# Como o tempo de permanência é o fator crucial para pacientes internados,
+# desejamos descobrir se o tempo de permanência pode ser previsto a partir de idade, gênero e raça.
+
+# Faremos a previsão com um modelo de Regressão Linear
+# Variável dependente: TempoInternaçao
+# Variáveis independentes: Idade, Sexo, Raça
+
+# H0 - Não há relação linear entre as variáveis dependente e independentes
+# H1 - Há relação linear entre as variáveis dependente e independentes
+
+modelo = lm(TempoInternaçao ~ Idade + Sexo + Raça, data = dados)
+summary(modelo)
+
+# Conclusão:
+# Valor-p > 0.05 para todas as variáveis independentes, logo, falhamos em rejeitar a H0
+# O tempo de permanência não pode ser previsto a partir da idade, gênero e raça.
+
+# Pergunta 7
+# Quais variáveis têm maior impacto nos custos de internação hospitalar?
+
+# Faremos a previsão com um modelo de Regressão Linear novamente
+# Variável dependente: CustoInternaçao
+# Variáveis independentes: Idade, Sexo, Raça, TempoInternaçao, GrupoDiagnosticoRefinado
+
+# H0 - Não há relação linear entre as variáveis dependente e independentes
+# H1 - Há relação linear entre as variáveis dependente e independentes
+
+modelo_v2 = lm(CustoInternaçao ~ ., data = dados)
+summary(modelo_v2)
+
+# As variáveis que possuem o maior impacto no custo de internação hospitalar são as
+# variáveis Idade, TempoInternaçao e GrupoDiagnosticoRefinado.
+# Vemos também que a variável Raça não é significativa.
+# Vamos removê-la e construir um novo modelo
+
+modelo_v3 = lm(CustoInternaçao ~ Idade + Sexo + TempoInternaçao + GrupoDiagnosticoRefinado, data = dados)
+summary(modelo_v3)
+
+# A variável Sexo não possui grande significância estatística.
+# Vamos removê-la e construir um novo modelo
+
+modelo_v4 = lm(CustoInternaçao ~ Idade + TempoInternaçao + GrupoDiagnosticoRefinado, data = dados)
+summary(modelo_v4)
+
+# Agora temos um modelo com todas as variáveis com alta significância
+# Porém, a variavel GrupoDiagnosticoRefinado possui valor t negativo
+# Vamos removê-la e construir um novo modelo
+
+modelo_v5 = lm(CustoInternaçao ~ Idade + TempoInternaçao, data = dados)
+summary(modelo_v5)
+
+# A remoção da variável GrupoDiagnosticoRefinado aumentou o erro padrão do modelo
+# Portanto, vamos descartá-lo e utilizar o melho modelo que encontramos, o modelo_v4
+
+summary(modelo_v4)
+
+# As variáveis que possuem o maior impacto no custo de internação hospitalar são as
+# variáveis Idade, TempoInternaçao e GrupoDiagnosticoRefinado.
